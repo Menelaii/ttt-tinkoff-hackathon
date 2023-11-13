15ab21a7f437c8500726183b826a8e4827230292
d.zhezha <d.post2434@gmail.com>
2023-11-13 13:07:29 +0300

    init

---
+++ src/main/java/ru/ttttinkoffhackathon/TttTinkoffHackathonApplication.java
@@ -0,0 +1,13 @@
+package ru.ttttinkoffhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TttTinkoffHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TttTinkoffHackathonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ttttinkoffhackathon/TttTinkoffHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ttttinkoffhackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TttTinkoffHackathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
